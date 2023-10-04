731dc2d004dc172d56699cfe5c38ea74d95a61d6
houcha <houcha@github.com>
2023-10-04 14:16:50 +0000

    Add CRF

--- parsers/src/feedforward_classifier.py
+++ parsers/src/feedforward_classifier.py
@@ -4,14 +4,16 @@ from typing import Dict, List, Optional
 import torch
 from torch import nn
 from torch import Tensor
+from torch.nn.utils.rnn import pad_sequence
 
 from allennlp.models import Model
 from allennlp.nn.activations import Activation
 from allennlp.training.metrics import CategoricalAccuracy, FBetaVerboseMeasure
-from allennlp.data.vocabulary import DEFAULT_OOV_TOKEN
+from allennlp.data.vocabulary import DEFAULT_OOV_TOKEN, DEFAULT_PADDING_TOKEN
+from allennlp.modules.conditional_random_field.conditional_random_field import ConditionalRandomField
 
 from .vocabulary import VocabularyWeighted
-from .cross_entropy import CrossEntropy
+from .metrics import CrossEntropy, IdentityLoss
 
 
 @Model.register('feed_forward_classifier')
@@ -27,7 +29,8 @@ class FeedForwardClassifier(Model):
         activation: str,
         dropout: float,
         use_class_weights: bool = False,
-        head_loss_weight: float = 1.0
+        head_loss_weight: float = 1.0,
+        use_crf = False
     ):
         super().__init__(vocab)
 
@@ -44,7 +47,14 @@ class FeedForwardClassifier(Model):
         weight_vector = None
         if use_class_weights:
             weight_vector = vocab.get_weight_vector(labels_namespace)
-        self.loss = CrossEntropy(weight=weight_vector)
+
+        if use_crf:
+            self.crf = ConditionalRandomField(n_classes)
+            self.loss = IdentityLoss()
+        else:
+            self.crf = None
+            self.loss = CrossEntropy(weight=weight_vector)
+
         self.head_loss_weight = head_loss_weight
         self.accuracy = CategoricalAccuracy()
         self.fscore = FBetaVerboseMeasure()
@@ -58,17 +68,25 @@ class FeedForwardClassifier(Model):
     ) -> Dict[str, Tensor]:
 
         logits = self.classifier(embeddings)
-        preds = logits.argmax(-1)
 
-        loss = torch.tensor(0.)
+        prediction_ids = logits.argmax(-1)
+        if self.crf is not None and not self.training:
+            paths_and_scores = self.crf.viterbi_tags(logits, mask)
+            paths_tensors = [torch.Tensor(path) for path, score in paths_and_scores]
+            padding_value = self.vocab.get_token_index(DEFAULT_PADDING_TOKEN)
+            prediction_ids = pad_sequence(paths_tensors, batch_first=True, padding_value=padding_value)
+
+        loss = torch.tensor(0.0)
         if labels is not None:
-            loss = self.update_loss(logits, labels, mask)
+            if self.crf is not None:
+                log_likelihood = self.crf(logits, labels, mask)
+                loss = self.loss(-log_likelihood) * self.head_loss_weight
+                # print(f"crf loss: {loss}")
+            else:
+                loss = self.loss(logits, labels, mask) * self.head_loss_weight
             self.update_metrics(logits, labels, mask)
 
-        return {'logits': logits, 'prediction_ids': preds, 'loss': loss}
-
-    def update_loss(self, logits: Tensor, target: Tensor, mask: Tensor) -> Tensor:
-        return self.loss(logits, target, mask) * self.head_loss_weight
+        return {'logits': logits, 'prediction_ids': prediction_ids, 'loss': loss}
 
     def update_metrics(self, logits: Tensor, target: Tensor, mask: Tensor):
         self.accuracy(logits, target, mask)
@@ -82,11 +100,11 @@ class FeedForwardClassifier(Model):
             "loss": self.loss.get_metric(reset)
         }
 
-    def decode_ids(self, predictions_ids: Tensor, oov_token_replacement=None) -> List[List]:
-        batch_size, sentence_max_length = predictions_ids.shape
+    def decode_ids(self, prediction_ids: Tensor, oov_token_replacement=None) -> List[List]:
+        batch_size, sentence_max_length = prediction_ids.shape
         predictions = [[None for i in range(sentence_max_length)] for j in range(batch_size)]
-        predictions_ids = predictions_ids.tolist()
-        for i, sentence_predictions_ids in enumerate(predictions_ids):
+        prediction_ids = prediction_ids.tolist()
+        for i, sentence_predictions_ids in enumerate(prediction_ids):
             for j, prediction_id in enumerate(sentence_predictions_ids):
                 predictions[i][j] = self.vocab.get_token_from_index(prediction_id, self.labels_namespace)
                 if oov_token_replacement is not None and predictions[i][j] == DEFAULT_OOV_TOKEN:

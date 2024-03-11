8fa96e558d11979faf622ec8b30efa11665cc794
Ilia <bail@bail-23.10>
2024-03-11 22:13:49 +0100

    Update conllu validation script

--- evaluate/validation/validate_conllu.py
+++ evaluate/validation/validate_conllu.py
@@ -6,8 +6,9 @@ from tqdm import tqdm
 
 from typing import Dict, Iterable
 
-sys.path.insert(0,'..')
-from semarkup import parse_semarkup, Sentence
+sys.path.append("../..") # import 'common' package
+from common.parse_conllu import parse_conllu_incr
+from common.sentence import Sentence
 
 
 def load_dict_from_json(json_file_path: str) -> Dict:
@@ -26,18 +27,8 @@ def is_int(s: str):
     else:
         return True
 
-def is_null(s: str):
-    try:
-        x, y = s.split('.')
-        if y != '1':
-            return False
-    except ValueError:
-        return False
-    else:
-        return True
 
-
-def validate_semarkup(sentences: Iterable[Sentence], vocab_file: str = None) -> bool:
+def validate_conllu(sentences: Iterable[Sentence], vocab_file: str = None) -> bool:
     if vocab_file is not None:
         vocab = load_dict_from_json(vocab_file)
         vocab["upos"] = set(vocab["upos"])
@@ -59,15 +50,14 @@ def validate_semarkup(sentences: Iterable[Sentence], vocab_file: str = None) -> 
             roots_count = 0
             
             for token in sentence:
-                sentence_all_ids.add(token.id)
+                sentence_all_ids.add(int(token.id))
                 if is_int(token.id):
                     sentence_natural_ids.add(int(token.id))
 
                 # Null validation
-                if "#null" in token.form.lower():
+                if token.is_null():
                     assert token.form == "#NULL", f"Did you mean #NULL?: {token.form}"
-                    assert token.head == '_', f"Null's head = {token.head} != _"
-                    token.head = None
+                    assert token.head == -1, f"Null's head = {token.head} != _"
                     assert token.deprel == '_', f"Null's deprel = {token.deprel} != _"
                     assert len(token.deps) > 0, f"Null's deps = {token.deps} != _"
                     assert token.misc == 'ellipsis', f"Null's misc != ellipsis"
@@ -93,7 +83,7 @@ def validate_semarkup(sentences: Iterable[Sentence], vocab_file: str = None) -> 
 
                 # Head
                 # Non-null tokens must have non-empty head.
-                assert is_int(token.head), f"Non-null tokens must have integer head."
+                assert is_int(token.head), f"Non-null tokens must have integer head. Encountered: {token.head} at {token.id}"
                 token.head = int(token.head)
                 if is_int(token.id):
                     assert token.head != int(token.id), f"Self-loops are not allowed in heads. Head: {token.head}"
@@ -103,13 +93,15 @@ def validate_semarkup(sentences: Iterable[Sentence], vocab_file: str = None) -> 
                 sentence_heads.add(token.head)
 
                 # Deps
-                for head, rels in token.deps.items():
-                    assert is_int(head) or is_null(head), \
+                token_heads = set()
+                for head, rel in token.deps.items():
+                    assert is_int(head) or head is None, \
                         f"Deps head must be either int or null (x.1). Encountered: {head}"
                     assert head != token.id, \
                         f"Self-loops are not allowed in deps. Head: {head}"
                     assert 0 <= float(head), \
                         f"Deps head must be non-negative. Encountered: {head}"
+                    token_heads.add(head)
                     sentence_deps_heads.add(head)
 
                 # Semslot
@@ -126,8 +118,9 @@ def validate_semarkup(sentences: Iterable[Sentence], vocab_file: str = None) -> 
             assert not ids_diff, f"Sentence ids are non-continuous, absent ids: {ids_diff}"
             head_ids_diff = sentence_heads - sentence_natural_ids - {0}
             assert not head_ids_diff, f"Heads' ids are not consistent with ids, extra ids: {head_ids_diff}"
-            deps_head_ids_diff = sentence_deps_heads - sentence_all_ids - {'0'}
-            assert not deps_head_ids_diff, f"Deps heads' ids are not consistent with ids, extra ids: {deps_head_ids_diff}"
+            deps_head_ids_diff = sentence_deps_heads - sentence_all_ids - {0}
+            assert not deps_head_ids_diff, f"Deps heads' ids are not consistent with ids, extra ids: {deps_head_ids_diff}"\
+                f"sentence_all_ids: {sentence_all_ids}, sentence_deps_heads: {sentence_deps_heads}"
             assert roots_count == 1, "There must be one ROOT (head=0) in a sentence."
 
         except AssertionError as e:
@@ -138,29 +131,29 @@ def validate_semarkup(sentences: Iterable[Sentence], vocab_file: str = None) -> 
     return is_valid
 
 
-def main(semarkup_file_path: str, vocab_file: str) -> None:
+def main(file_path: str, vocab_file: str) -> None:
     print(f"Load sentences...")
-    with open(semarkup_file_path, "r", encoding='utf8') as semarkup_file:
-        sentences = parse_semarkup(semarkup_file, incr=True)
-        if validate_semarkup(sentences, vocab_file):
+    with open(file_path, "r", encoding='utf8') as file:
+        sentences = parse_conllu_incr(file)
+        if validate_conllu(sentences, vocab_file):
             print("Seems legit!")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='SEMarkup sanity check.')
+    parser = argparse.ArgumentParser(description='Conllu sanity check.')
     parser.add_argument(
-        'semarkup_file',
+        'conllu_file',
         type=str,
-        help='SEMarkup file to check.'
+        help='Conllu file to check.'
     )
     parser.add_argument(
         '-vocab_file',
         type=str,
         help="JSON file with ground-truth vocabulary (use build_vocab.py to build one)."
-        "For example, you can use it if you have a correct train SEMarkup file and "
-        "want to make sure test SEMarkup file doesn't have tags which are not present in test (OOV).",
+        "For example, you can use it if you have a correct train conllu file and "
+        "want to make sure test the file doesn't have tags that are absent from test (OOV).",
         default=None
     )
     args = parser.parse_args()
-    main(args.semarkup_file, args.vocab_file)
+    main(args.conllu_file, args.vocab_file)
 

88373aee40d8d9a30515eec7ef91cc23565be59b
Ilia <bail@bail-23.10>
2024-04-11 01:00:36 +0200

    Fix feats serialization

--- common/token.py
+++ common/token.py
@@ -35,22 +35,31 @@ class Token:
     def serialize(self) -> str:
         serialize_field = lambda x: conllu.serializer.serialize_field(x) if x is not None else ''
 
-        # Custom serialization for 'head' tag.
-        head = None
-        if self.head is None:
-            head = ''
-        elif self.head == -1:
-            head = '_'
+        # Custom serialization for 'feats' tag.
+        if self.feats is None:
+            feats_str = ''
         else:
-            head = str(self.head)
+            assert type(self.feats) is dict
+            if len(self.feats) == 0:
+                feats_str = '_'
+            else:
+                feats_str = serialize_field(self.feats)
+        
+        # Custom serialization for 'head' tag.
+        if self.head is None:
+            head_str = ''
+        elif self.head == -1:
+            head_str = '_'
+        else:
+            head_str = str(self.head)
 
         # Custom serialization for 'deps' tag.
         if self.deps is None:
-            deps = ''
+            deps_str = ''
         elif len(self.deps) == 0:
-            deps = '_'
+            deps_str = '_'
         else:
-            deps = '|'.join(f"{head}:{rel}" for head, rels in self.deps.items() for rel in rels)
+            deps_str = '|'.join(f"{head}:{rel}" for head, rels in self.deps.items() for rel in rels)
 
         return (
             f"{serialize_field(self.id)}\t"
@@ -58,10 +67,10 @@ class Token:
             f"{serialize_field(self.lemma)}\t"
             f"{serialize_field(self.upos)}\t"
             f"{serialize_field(self.xpos)}\t"
-            f"{serialize_field(self.feats)}\t"
-            f"{head}\t"
+            f"{feats_str}\t"
+            f"{head_str}\t"
             f"{serialize_field(self.deprel)}\t"
-            f"{deps}\t"
+            f"{deps_str}\t"
             f"{serialize_field(self.misc)}\t"
             f"{serialize_field(self.semslot)}\t"
             f"{serialize_field(self.semclass)}"
